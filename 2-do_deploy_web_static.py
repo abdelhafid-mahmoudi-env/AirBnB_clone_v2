8b8d851c5faa57967af9eeb07d0b9da48bcf01eb
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-04-04 02:12:54 -0700

    test

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,37 +1,34 @@
 #!/usr/bin/python3
-"""Deploys an archive to web servers."""
+""" Does deployment"""
 
-from fabric.api import put, run, env
+from fabric.api import *
 import os
 
-# Set the environment hosts and user for deployment
-env.hosts = ["52.23.177.252", "18.204.7.7"]
+env.hosts = ["34.73.8.171", "34.74.18.52"]
 env.user = "ubuntu"
 
+
 def do_deploy(archive_path):
-    """Deploys archive to servers after validating the path and extension."""
-    # Validate the archive path exists and has a .tgz extension
-    if not os.path.isfile(archive_path) or not archive_path.endswith('.tgz'):
+    """ Deploys archive to servers"""
+    if not os.path.exists(archive_path):
         return False
 
-    try:
-        # Transfer the archive to the remote server
-        res = put(archive_path, "/tmp/")
-        if not res.succeeded:
-            return False
+    results = []
 
-        basename = os.path.basename(archive_path)
-        name = basename[:-4]  # Remove .tgz extension
-        new_dir = f"/data/web_static/releases/{name}"
+    res = put(archive_path, "/tmp")
+    results.append(res.succeeded)
 
-        # Execute deployment steps
-        run(f"mkdir -p {new_dir}")
-        run(f"tar -xzf /tmp/{basename} -C {new_dir}")
-        run(f"rm /tmp/{basename}")
-        run(f"mv {new_dir}/web_static/* {new_dir}")
-        run(f"rm -rf {new_dir}/web_static")
-        run("rm -rf /data/web_static/current")
-        run(f"ln -s {new_dir} /data/web_static/current")
-    except Exception as e:
-        return False
+    basename = os.path.basename(archive_path)
+    if basename[-4:] == ".tgz":
+        name = basename[:-4]
+    newdir = "/data/web_static/releases/" + name
+    run("mkdir -p " + newdir)
+    run("tar -xzf /tmp/" + basename + " -C " + newdir)
+
+    run("rm /tmp/" + basename)
+    run("mv " + newdir + "/web_static/* " + newdir)
+    run("rm -rf " + newdir + "/web_static")
+    run("rm -rf /data/web_static/current")
+    run("ln -s " + newdir + " /data/web_static/current")
+
     return True
