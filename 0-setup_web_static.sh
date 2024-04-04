9afbfa2ea3fa00188214aa9ba8a1f323c9780aaf
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-04-04 00:30:53 -0700

    setup web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/env bash
-#sets up the web servers
+# This script sets up the web servers for deployment of web_static
 
-sudo apt-get -y update
-sudo apt-get -y upgrade
-sudo apt-get -y install nginx
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "This is a test" | sudo tee /data/web_static/releases/test/index.html
+# Install Nginx if not already installed
+if ! command -v nginx &>/dev/null; then
+    sudo apt-get -y update
+    sudo apt-get -y upgrade
+    sudo apt-get -y install nginx
+fi
+
+# Create necessary directories
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared /data/web_static/current
+
+# create file named index.html
+sudo touch /data/web_static/releases/test/index.html
+
+# Create a fake HTML file
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
