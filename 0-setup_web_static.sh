146fef00569684bb8c112c82475176c573d56724
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-04-04 00:24:53 -0700

    setup web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,40 +1,51 @@
 #!/usr/bin/env bash
-# This script sets up the web servers for deployment of web_static
+# Deploy static AirBnB webserver suing this bash script
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo mkdir -p /data/ /data/web_static/ /data/web_static/releases/ /data/web_static/shared/ /data/web_static/releases/test/
+echo "Holberton School for the win!" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+sudo rm -rf /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+NEW_STRING="\\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n"
+sudo sed -i "38i $NEW_STRING" /etc/nginx/sites-available/default
+sudo service nginx restart
+#!/usr/bin/env bash
+# Sets up the web servers for the deployment of web_static
 
-# Install Nginx if not already installed
-if ! command -v nginx &>/dev/null; then
-    sudo apt-get -y update
-    sudo apt-get -y upgrade
-    sudo apt-get install -y nginx
-    sudo ufw allow 'Nginx HTTP'
-fi
+# Update package lists
+apt update
 
-# Remove existing directory if it exists
-sudo rm -rf /data/web_static/
+# Install Nginx
+apt install -y nginx
 
-# Create necessary directories
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared /data/web_static/current
+# - Create the folders, if they don't yet exist:
+#   * '/data'
+#   * '/data/web_static/'
+#   * '/data/web_static/releases/'
+#   * '/data/web_static/releases/test/'
+#   * '/data/web_static/shared/'
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
 
-# create file named index.html
-sudo touch /data/web_static/releases/test/index.html
+# Create a fake HTML file '/data/web_static/releases/test/index.html',
+# (with simple content, to test Nginx configuration)
+printf "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>\n" | 
+tee /data/web_static/releases/test/index.html 
 
-# Create a fake HTML file
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+# Create a symbolic link '/data/web_static/current' linked to the
+# '/data/web_static/releases/test/' folder.
+ln -fs /data/web_static/releases/test/ /data/web_static/current
 
-# Create symbolic link, deleting and recreating if already exists
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Give recursive ownership of the '/data/' folder to the 'ubuntu' user AND group
+chown -R ubuntu:ubuntu /data/
 
-# Give ownership of /data/ to ubuntu user and group
-sudo chown -hR ubuntu:ubuntu /data/
-
-# Update Nginx configuration
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+# Update the Nginx configuration to serve the content of '/data/web_static/current/'
+# to 'hbnb_static' (ex: https://mydomainname.tech/hbnb_static).
+loc_header="location \/hbnb\_static\/ {"
+loc_content="alias \/data\/web\_static\/current\/;"
+new_location="\n\t$loc_header\n\t\t$loc_content\n\t}\n"
+sed -i "37s/$/$new_location/" /etc/nginx/sites-available/default
 
 # Restart Nginx
-sudo service nginx restart
+service nginx restart
