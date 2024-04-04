f8010eec9b56ff83ec1f9e8f5d2fb6a6fc2dfd07
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-04-04 00:42:37 -0700

    setup web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -12,7 +12,7 @@ fi
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 
 # Create a fake HTML file
-echo "<html>\n\t\<head>\n\t\</head>\n\t\<body>\n\t\Holberton School\n\t\</body>\n\t\</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+echo "<html>\n\t\<head>\n\t\</head>\n\t\<body>\n\t\Holberton School\n\t\</body>\n\t\</html>" | sudo tee /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
