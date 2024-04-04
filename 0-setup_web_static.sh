cb4af448bfc902dc25adc3865d9f026ccf19fce2
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-04-04 00:09:10 -0700

    setup web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -34,8 +34,7 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 
 # Update Nginx configuration
-config="location /hbnb_static { alias /data/web_static/current/; index index.html index.htm; }"
-sudo sed -i "/^http {/a $config" /etc/nginx/sites-available/default
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
 # Restart Nginx
 sudo service nginx restart
