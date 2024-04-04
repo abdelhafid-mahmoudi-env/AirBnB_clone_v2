5eda54b83794e288093fb30852c088f782fdbc45
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-04-04 03:04:32 -0700

    test

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,36 +1,34 @@
 #!/usr/bin/python3
 """This module defines a Fabric script."""
 
-from fabric.api import env, put, run
-from os.path import exists
+from fabric.api import run, env, put
+import os.path
 
 # Update with your server IPs
 env.hosts = ['52.23.177.252', '18.204.7.7']
+env.key_filename = '~/.ssh/school'
+env.user = 'ubuntu'
 
 
 def do_deploy(archive_path):
     """Deploys the archive to the web servers."""
 
-    if not exists(archive_path):
+    if not os.path.isfile(archive_path):
         return False
+    compressed_file = archive_path.split("/")[-1]
+    no_extension = compressed_file.split(".")[0]
+
     try:
-        file_name = archive_path.split("/")[-1]
-        name = file_name.split(".")[0]
-        put(archive_path, f'/tmp/{file_name}')
-        run(f'mkdir -p /data/web_static/releases/{name}/')
-        run(
-            f'tar -xzf /tmp/{file_name} -C /data/web_static/releases/{name}/'
-        )
-        run(f'rm /tmp/{file_name}')
-        run(
-            f'mv /data/web_static/releases/{name}/web_static/* '
-            f'/data/web_static/releases/{name}/'
-        )
-        run(f'rm -rf /data/web_static/releases/{name}/web_static')
-        run('rm -rf /data/web_static/current')
-        run(
-            f'ln -s /data/web_static/releases/{name}/ /data/web_static/current'
-        )
+        remote_path = "/data/web_static/releases/{}/".format(no_extension)
+        sym_link = "/data/web_static/current"
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(remote_path))
+        run("sudo tar -xvzf /tmp/{} -C {}".format(compressed_file, remote_path))
+        run("sudo rm /tmp/{}".format(compressed_file))
+        run("sudo mv {}/web_static/* {}".format(remote_path, remote_path))
+        run("sudo rm -rf {}/web_static".format(remote_path))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -sf {} {}".format(remote_path, sym_link))
         return True
-    except Exception:
+    except Exception as e:
         return False
