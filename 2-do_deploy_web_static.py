d596dcf5c0cd62408e2b32be8c04df37f3de37d1
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-04-04 02:53:35 -0700

    test

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
-"""
-This module defines a Fabric script that distributes an archive to web servers.
-"""
+"""This module defines a Fabric script."""
 
 from fabric.api import env, put, run
 from os.path import exists
 
-env.hosts = ['52.23.177.252', '	18.204.7.7']  # Update with your server IPs
+# Update with your server IPs
+env.hosts = ['52.23.177.252', '18.204.7.7']
+
 
 def do_deploy(archive_path):
     """
@@ -20,24 +20,32 @@ def do_deploy(archive_path):
         name = file_name.split(".")[0]
 
         # Upload the archive to the /tmp/ directory of the web server
-        put(archive_path, '/tmp/{}'.format(file_name))
+        put(archive_path, f'/tmp/{file_name}')
 
         # Uncompress the archive to the folder on the web server
-        run('mkdir -p /data/web_static/releases/{}/'.format(name))
-        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'.format(file_name, name))
+        run(f'mkdir -p /data/web_static/releases/{name}/')
+        run(
+            f'tar -xzf /tmp/{file_name} -C /data/web_static/releases/{name}/'
+        )
 
         # Delete the archive from the web server
-        run('rm /tmp/{}'.format(file_name))
+        run(f'rm /tmp/{file_name}')
 
-        # Move the content of the web static to the parent directory and delete the directory
-        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(name, name))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(name))
+        # Move the content of the web static to the parent directory and
+        # delete the directory
+        run(
+            f'mv /data/web_static/releases/{name}/web_static/* '
+            f'/data/web_static/releases/{name}/'
+        )
+        run(f'rm -rf /data/web_static/releases/{name}/web_static')
 
         # Delete the symbolic link and create a new one
         run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(name))
+        run(
+            f'ln -s /data/web_static/releases/{name}/ /data/web_static/current'
+        )
 
         print("New version deployed!")
         return True
-    except Exception as e:
+    except Exception:
         return False
