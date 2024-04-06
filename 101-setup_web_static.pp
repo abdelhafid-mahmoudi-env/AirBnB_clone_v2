aa828447b80f45d72c8c012de95ab27f9618a831
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-04-06 04:24:37 -0700

    test

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,13 +1,37 @@
-# Puppet manifest to setup web servers for deployment of web_static
+# Puppet manifest to set up web servers for deployment of web_static
 
-file { '/etc/nginx/sites-available/default':
-    ensure => file,
-    content => template('nginx-default.erb'),
-    require => Package['nginx'],
-    notify  => Service['nginx'],
+# Define parameters
+$nginx_config_file = '/etc/nginx/sites-available/default'
+$web_static_dir = '/data/web_static'
+$release_dir = "${web_static_dir}/releases/test"
+$index_file_path = "${release_dir}/index.html"
+$nginx_service = 'nginx'
+$nginx_package = 'nginx'
+$web_owner = 'ubuntu'
+$web_group = 'ubuntu'
+
+# Install Nginx package
+package { $nginx_package:
+    ensure => installed,
 }
 
-file { '/data/web_static/releases/test/index.html':
+# Ensure Nginx configuration file is present and correctly configured
+file { $nginx_config_file:
+    ensure => file,
+    content => template('nginx-default.erb'),
+    require => Package[$nginx_package],
+    notify  => Service[$nginx_service],
+}
+
+# Create the directory structure for web_static
+file { [$web_static_dir, $release_dir]:
+    ensure => directory,
+    owner  => $web_owner,
+    group  => $web_group,
+}
+
+# Create index.html file for the test deployment
+file { $index_file_path:
     ensure  => file,
     content => "<html>
   <head>
@@ -16,18 +40,22 @@ file { '/data/web_static/releases/test/index.html':
     Holberton School
   </body>
 </html>",
-    require => File['/data/web_static/releases/test'],
-    notify  => Service['nginx'],
+    owner   => $web_owner,
+    group   => $web_group,
+    require => File[$release_dir],
+    notify  => Service[$nginx_service],
 }
 
-file { '/data/web_static/current':
+# Create a symbolic link to the current deployment
+file { "${web_static_dir}/current":
     ensure => link,
-    target => '/data/web_static/releases/test',
-    require => File['/data/web_static/releases/test/index.html'],
-    notify  => Service['nginx'],
+    target => $release_dir,
+    require => File[$index_file_path],
+    notify  => Service[$nginx_service],
 }
 
-exec { 'chown-data':
-    command => 'chown -R ubuntu:ubuntu /data/',
-    path    => ['/bin', '/usr/bin/'],
+# Change ownership of the /data directory recursively to the ubuntu user
+exec { 'chown-web':
+    command => "chown -R ${web_owner}:${web_group} ${web_static_dir}",
+    onlyif  => "test ! -w ${web_static_dir}",
 }
