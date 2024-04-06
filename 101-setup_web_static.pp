6165b675500e98d4e112afe78d9a139a7c780946
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-04-06 08:37:41 -0700

    test

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,58 +1,15 @@
-# 101-setup_web_static.pp
-
-# Ensure Nginx package is installed
-package { 'nginx':
-  ensure => installed,
-}
-
-# Define directories
-file { '/data':
-  ensure => directory,
-}
-
-file { '/data/web_static':
-  ensure => directory,
-  owner  => 'root',
-  group  => 'root',
-}
-
-file { '/data/web_static/releases':
-  ensure => directory,
-  owner  => 'root',
-  group  => 'root',
-}
-
-file { '/data/web_static/shared':
-  ensure => directory,
-  owner  => 'root',
-  group  => 'root',
-}
-
-file { '/data/web_static/releases/test':
-  ensure => directory,
-  owner  => 'root',
-  group  => 'root',
-}
-
-# Create a fake HTML file for testing
-file { '/data/web_static/releases/test/index.html':
-  ensure  => file,
-  content => '<html><head></head><body>Holberton School</body></html>',
-  owner   => 'root',
-  group   => 'root',
-}
-
-# Create a symbolic link
-file { '/data/web_static/current':
-  ensure  => link,
-  target  => '/data/web_static/releases/test',
-  owner   => 'root',
-  group   => 'root',
-}
-
-# Restart Nginx service
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/data/web_static/current'],
-}
+# a puppet version of out setup of nginx for web static
+exec { '/usr/bin/env apt-get -y update' : }
+-> exec { '/usr/bin/env apt-get -y install nginx' : }
+-> exec { '/usr/bin/env sed -i "/listen \[::\]:80 default_server/ a\\\trewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default' : }
+-> exec { '/usr/bin/env sed -i "/listen \[::\]:80 default_server/ a\\\tadd_header X-Served-By \"\$HOSTNAME\";" /etc/nginx/sites-available/default' : }
+-> exec { '/usr/bin/env sed -i "/redirect_me/ a\\\terror_page 404 /custom_404.html;" /etc/nginx/sites-available/default' : }
+-> exec { '/usr/bin/env # echo "Ceci n\'est pas une page" > /var/www/html/custom_404.html' : }
+-> exec { '/usr/bin/env service nginx start' : }
+-> exec { '/usr/bin/env mkdir -p /data/web_static/releases/test/' : }
+-> exec { '/usr/bin/env mkdir -p /data/web_static/shared/' : }
+-> exec { '/usr/bin/env echo "simple content, to test your Nginx configuration" > /data/web_static/releases/test/index.html' : }
+-> exec { '/usr/bin/env ln -sf /data/web_static/releases/test/ /data/web_static/current' : }
+-> exec { '/usr/bin/env sed -i "/^\tlocation \/ {$/ i\\\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t\tautoindex off;\n}" /etc/nginx/sites-available/default' : }
+-> exec { '/usr/bin/env service nginx restart' : }
+-> exec { '/usr/bin/env chown -R ubuntu:ubuntu /data/' : }
