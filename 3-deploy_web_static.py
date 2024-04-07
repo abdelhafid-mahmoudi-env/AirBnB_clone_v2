b0cc24eea870bc28c4d8d81993b3169986b04482
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-04-07 11:34:17 -0700

    test

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-    Fabric script that creates and distributes an archive
-    on my web servers, using deploy function
-"""
+""" Fabric script that creates and distributes an archive. """
 from fabric.api import *
 from fabric.operations import run, put, sudo, local
 from datetime import datetime
@@ -13,14 +10,12 @@ created_path = None
 
 
 def do_pack():
-    """
-        generates a .tgz archine from contents of web_static
-    """
+    """generates a .tgz archine from contents of web_static"""
     time = datetime.utcnow().strftime('%Y%m%d%H%M%S')
     file_name = "versions/web_static_{}.tgz".format(time)
     try:
         local("mkdir -p ./versions")
-        local("tar --create --verbose -z --file={} ./web_static"
+        local("tar -cvzf {} web_static"
               .format(file_name))
         return file_name
     except Exception as e:
