4e7effa4649b5986f3f86a4f07b08a838249fd3b
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-04-07 15:35:16 -0700

    test

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -12,10 +12,7 @@ from fabric.api import put, env, run, local
 from datetime import datetime
 import os
 
-# List of remote servers
 env.hosts = ["52.23.177.252", "18.204.7.7"]
-
-# Username for SSH login
 env.user = "ubuntu"
 
 
@@ -25,14 +22,14 @@ def do_pack():
     Returns:
         str: File path of the compressed archive on success, None on failure.
     """
-    try:
-        now = datetime.now().strftime("%Y%m%d%H%M%S")
-        archive_path = "versions/web_static_{}.tgz".format(now)
-        local("mkdir -p versions")
-        local("tar -czvf {} web_static".format(archive_path))
-        return archive_path
-    except Exception:
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    archive_path = "versions/web_static_{}.tgz".format(now)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -czvf {} web_static".format(archive_path)).failed is True:
         return None
+    return (archive_path)
 
 
 def do_deploy(archive_path):
@@ -50,17 +47,25 @@ def do_deploy(archive_path):
     aname = os.path.basename(archive_path)
     rname = aname.split(".")[0]
 
-    put(local_path=archive_path, remote_path="/tmp/")
-    run("mkdir -p /data/web_static/releases/{}".format(rname))
-    run("tar -xzf /tmp/{} \
-        -C /data/web_static/releases/{}".format(aname, rname))
-    run("rm /tmp/{}".format(aname))
-    run("rm -rf /data/web_static/current")
-    run("ln -fs /data/web_static/releases/{}/ \
-        /data/web_static/current".format(rname))
-    run("mv /data/web_static/current/web_static/* \
-        /data/web_static/current/")
-    run("rm -rf /data/web_static/current/web_static")
+    if put(local_path=archive_path, remote_path="/tmp/").failed is True:
+        return True
+    if run("mkdir -p /data/web_static/releases/{}".format(rname)).failed is True:
+        return True
+    if run("tar -xzf /tmp/{} \
+        -C /data/web_static/releases/{}".format(aname, rname)).failed is True:
+        return True
+    if run("rm /tmp/{}".format(aname)).failed is True:
+        return True
+    if run("rm -rf /data/web_static/current").failed is True:
+        return True
+    if run("ln -fs /data/web_static/releases/{}/ \
+        /data/web_static/current".format(rname)).failed is True:
+        return True
+    if run("mv /data/web_static/current/web_static/* \
+        /data/web_static/current/").failed is True:
+        return True
+    if run("rm -rf /data/web_static/current/web_static").failed is True:
+        return True
     print("New version deployed!")
     return True
 
@@ -75,4 +80,6 @@ def deploy():
         bool: True if deployment is successful, False otherwise.
     """
     deploythis = do_deploy(archive)
+    if deploythis is None:
+        return False
     return (deploythis)
