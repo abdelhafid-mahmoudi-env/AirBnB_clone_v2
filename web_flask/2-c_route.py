8ab61c558b6f33a1d5a1c79de1447e60afe31676
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-04-21 09:51:51 -0700

    c routee

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,37 +1,40 @@
 #!/usr/bin/python3
 """
-Starts a Flask web application.
+This script initializes a Flask web application.
 """
 
-from flask import Flask, escape
+from flask import Flask
 
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
+def index():
     """
-    Displays "Hello HBNB!" when accessing the root URL.
+    Route: '/'
+    Displays 'Hello HBNB!' when accessing the root URL.
     """
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
-    Displays "HBNB" when accessing the /hbnb URL.
+    Route: '/hbnb'
+    Displays 'HBNB' when accessing the /hbnb URL.
     """
-    return 'HBNB'
+    return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
+def c(text):
     """
-    Displays "C " followed by the value of the text variable.
-    Replace underscore _ symbols with a space.
+    Route: '/c/<text>'
+    Displays 'C ' followed by the value of the text variable.
+    Replaces underscores (_) with spaces in the text.
     """
-    return 'C {}'.format(escape(text.replace('_', ' ')))
+    return "C {}".format(text.replace("_", " "))
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
