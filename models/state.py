0e05ea7fac0169630a0ea80eb51b1cd1bc1698f4
Abdelhafid Mahmoudi <abdelhafid.mahmoudi.env@gmail.com>
2024-04-21 10:37:33 -0700

    test

--- models/state.py
+++ models/state.py
@@ -4,25 +4,31 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 from models import my_enviroment
-from models.city import City
-from models import storage
 
 
 class State(BaseModel, Base):
     """Representation of state """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
     if my_enviroment == "db":
+        name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state")
     else:
-        @property
-        def cities(self):
-            """Returns the list"""
-            all_cities = storage.all(City)
-            state_cities = [city for city in all_cities.values()
-                    if city.state_id == self.id]
-            return state_cities
+        name = ""
 
     def __init__(self, *args, **kwargs):
         """initializes state"""
         super().__init__(*args, **kwargs)
+
+    if my_enviroment != "db":
+        @property
+        def cities(self):
+            """Returns the list of City object"""
+            from models import storage
+            from models.city import City
+
+            all_cities = storage.all(City)
+            state_cities = [
+                    city for city in all_cities.values()
+                    if city.state_id == self.id
+            ]
+            return state_cities
